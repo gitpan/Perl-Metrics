b3b6635e57e5f80ded03556db9c0f34be12f95fc
Adam Kennedy <adamk@cpan.org>
2008-11-01 12:18:34 +0000

    Import of ADAMK/Perl-Metrics-0.09 from CPAN.
    gitpan-cpan-distribution: Perl-Metrics
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         ADAMK/Perl-Metrics-0.09.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/Perl/Metrics.pm
+++ lib/Perl/Metrics.pm
@@ -65,7 +65,7 @@ all.
 
 =cut
 
-use 5.005;
+use 5.00503;
 use strict;
 use Carp                   ();
 use DBI                    ();
@@ -77,7 +77,7 @@ use Module::Pluggable;
 
 use vars qw{$VERSION $TRACE};
 BEGIN {
-	$VERSION = '0.08';
+	$VERSION = '0.09';
 	
 	# Enable the trace flag to show trace messages during the
 	# main processing loops in this class
@@ -107,7 +107,6 @@ use Perl::Metrics::CDBI   ();
 use Perl::Metrics::File   ();
 use Perl::Metrics::Metric ();
 use Perl::Metrics::Plugin ();
-
 
 
 
